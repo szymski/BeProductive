73a20d4dd3ffaf2d9d5bc9ab0a563925fc74df7f
Szymon Jankowski <s.jankowski@fireup.pro>
2021-10-30 04:05:10 +0200

    Change goal state from calendar

--- BeProductive/Modules/Common/Helpers/DateHelper.cs
+++ BeProductive/Modules/Common/Helpers/DateHelper.cs
@@ -7,10 +7,16 @@ public static class DateHelper
         new DateTime(date.Year, date.Month, 1),
         new DateTime(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month))
     );
-    
+
     public static (DateOnly firstDay, DateOnly lastDay) FirstAndLastDayOfMonth(DateOnly date) =>
     (
         new DateOnly(date.Year, date.Month, 1),
         new DateOnly(date.Year, date.Month, DateTime.DaysInMonth(date.Year, date.Month))
     );
+
+    public static bool IsSameDay(DateOnly date1, DateOnly date2) =>
+        AntDesign.DateHelper.IsSameDay(
+            date1.ToDateTime(TimeOnly.MinValue),
+            date2.ToDateTime(TimeOnly.MinValue)
+        );
 }
