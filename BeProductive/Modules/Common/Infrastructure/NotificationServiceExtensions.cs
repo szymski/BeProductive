0d7f82d793e2ebfdb6fb8a631158aa03effa4089
Szymon Jankowski <s.jankowski@fireup.pro>
2021-11-22 18:52:19 +0100

    Changed NotificationService to MessageService

--- BeProductive/Modules/Common/Infrastructure/NotificationServiceExtensions.cs
+++ BeProductive/Modules/Common/Infrastructure/NotificationServiceExtensions.cs
@@ -4,15 +4,15 @@ namespace BeProductive.Modules.Common.Infrastructure;
 
 public static class NotificationServiceExtensions
 {
-    public static void Success(this NotificationService service, string message) =>
-        service.Success(new() { Message = message });
+    public static void Success(this MessageService service, string message) =>
+        service.Success(message);
     
-    public static void Error(this NotificationService service, string message) =>
-        service.Error(new() { Message = message });
+    public static void Error(this MessageService service, string message) =>
+        service.Error(message);
     
-    public static void Warn(this NotificationService service, string message) =>
-        service.Warn(new() { Message = message });
+    public static void Warn(this MessageService service, string message) =>
+        service.Warn(message);
     
-    public static void Info(this NotificationService service, string message) =>
-        service.Info(new() { Message = message });
+    public static void Info(this MessageService service, string message) =>
+        service.Info(message);
 }
