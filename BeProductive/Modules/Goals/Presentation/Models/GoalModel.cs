a78cf80b20688a133cf072a0bf54d994f562803b
Szymon Jankowski <s.jankowski@fireup.pro>
2021-11-27 05:19:36 +0100

    Saving/editing allowed days

--- BeProductive/Modules/Goals/Presentation/Models/GoalModel.cs
+++ BeProductive/Modules/Goals/Presentation/Models/GoalModel.cs
@@ -10,14 +10,18 @@ public class GoalModel
 
     [Required]
     public string Color { get; set; } = GoalColors.RandomColor;
-    
+
     public string Icon { get; set; }
+
+    [Required]
+    public AllowedDaysOfWeek AllowedDaysOfWeek { get; set; }
 
     public static GoalModel FromGoal(Goal goal) => new()
     {
         Name = goal.Name,
         Color = goal.Color,
         Icon = goal.Icon,
+        AllowedDaysOfWeek = goal.AllowedDaysOfWeek,
     };
 
     public Goal ApplyToGoal(Goal goal)
@@ -25,6 +29,7 @@ public class GoalModel
         goal.Name = Name;
         goal.Color = Color;
         goal.Icon = Icon;
+        goal.AllowedDaysOfWeek = AllowedDaysOfWeek; 
         return goal;
     }
 }
