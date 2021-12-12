d630a766ed7380fe1186db051ac2c494cceef1e0
Szymon Jankowski <s.jankowski@fireup.pro>
2021-12-12 23:29:42 +0100

    Lottie animation wrapper component

---
+++ BeProductive/wwwroot/animations.js
@@ -0,0 +1,14 @@
+﻿window.LoadAnimation = function(path, containerId) {
+    const anim = bodymovin.loadAnimation({
+        container: document.getElementById(containerId),
+        path: path,
+        renderer: 'svg',
+        autoplay: false,
+        loop: false,
+    });
+    anim.playShot = () => {
+        anim.stop();
+        anim.play();
+    };
+    return anim;
+}
