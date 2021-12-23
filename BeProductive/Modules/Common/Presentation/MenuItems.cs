ec2ca8ea26630e49c6220189a843a7c20072b98a
Szymon Jankowski <s.jankowski@fireup.pro>
2021-12-23 02:33:46 +0100

    Separate file for configurtion sidebar menu items

---
+++ BeProductive/Modules/Common/Presentation/MenuItems.cs
@@ -0,0 +1,15 @@
+﻿namespace BeProductive.Modules.Common.Presentation; 
+
+public record MenuItem(string Name, string Route);
+
+public static class MenuItems {
+    public static readonly List<MenuItem> Items = new()
+    {
+        new("Goals", "/"),
+        new("Rituals", "/rituals"),
+        new("Timer", "/timer"),
+        new("To do", "/todo"),
+        new("Components", "/components"),
+        new("Account", "/account"),
+    };   
+}
