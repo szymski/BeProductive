2afecedd7370453979c58481504aba11bce52650
Szymon Jankowski <s.jankowski@fireup.pro>
2022-01-04 03:20:54 +0100

    Fixed account page bugs on postgres

--- BeProductive/Modules/Users/Infrastructure/UserService.cs
+++ BeProductive/Modules/Users/Infrastructure/UserService.cs
@@ -1,31 +1,40 @@
 ﻿using BeProductive.Modules.Users.Domain;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace BeProductive.Modules.Users.Infrastructure;
 
 public class UserService
 {
+    private readonly IDbContextFactory<AppContext> _contextFactory;
     private ILogger<UserService> _logger;
-    private AppContext _context;
     private AuthService _authService;
     private UserManager<User> _userManager;
 
     public UserService(
         ILogger<UserService> logger,
-        AppContext context,
         AuthService authService,
-        UserManager<User> userManager)
+        UserManager<User> userManager,
+        IDbContextFactory<AppContext> contextFactory)
     {
         _logger = logger;
-        _context = context;
         _authService = authService;
         _userManager = userManager;
+        _contextFactory = contextFactory;
+    }
+    
+    public async Task<User?> GetUserById(int id)
+    {
+        await using var context = await _contextFactory.CreateDbContextAsync();
+        return await context.Users.FindAsync(id);
     }
 
     public async Task ChangeUsername(string username)
     {
+        await using var context = await _contextFactory.CreateDbContextAsync();
+        
         var authData = await _authService.GetAuthDataAsync();
-        var user = await _context.Users.FindAsync(authData!.UserId);
+        var user = await _userManager.FindByIdAsync(authData!.UserId.ToString());
 
         if (await _userManager.FindByNameAsync(username) is not null)
         {
@@ -36,21 +45,23 @@ public class UserService
         _logger.LogInformation("Updating user {@User} username to {Username}", user, username);
         await _userManager.SetUserNameAsync(user, username);
 
-        await _context.SaveChangesAsync();
+        await context.SaveChangesAsync();
 
         await _authService.Login(user);
     }
     
     public async Task ChangePassword(string password)
     {
+        await using var context = await _contextFactory.CreateDbContextAsync();
+        
         var authData = await _authService.GetAuthDataAsync();
-        var user = await _context.Users.FindAsync(authData!.UserId);
+        var user = await context.Users.FindAsync(authData!.UserId);
 
         _logger.LogInformation("Updating user {@User} password", user);
         var token = await _userManager.GeneratePasswordResetTokenAsync(user);
         await _userManager.ResetPasswordAsync(user, token, password);
 
-        await _context.SaveChangesAsync();
+        await context.SaveChangesAsync();
 
         await _authService.Login(user);
     }
